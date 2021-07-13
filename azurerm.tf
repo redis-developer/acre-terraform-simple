d5db44f75cb522dd542cd4abfd3932bd880e514c
DaShaun <dashaun@redislabs.com>
2021-07-13 11:48:10 -0500

    remove required configurations to support use via CLI login

--- azurerm.tf
+++ azurerm.tf
@@ -1,12 +1,5 @@
 provider "azurerm" {
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  subscription_id = var.subscription_id
-  tenant_id       = var.tenant_id
-  environment     = var.cloud_name
+  environment = var.cloud_name
   features {
-    template_deployment {
-      delete_nested_items_during_deletion = false
-    }
   }
 }
