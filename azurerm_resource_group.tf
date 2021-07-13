86c27a64acb811310c03b7b4c24acae175042616
DaShaun <dashaun@redislabs.com>
2021-07-13 11:50:02 -0500

    use configured location and update tag

--- azurerm_resource_group.tf
+++ azurerm_resource_group.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "redisgeek" {
   name     = format("redisgeek-%s", random_string.resource_group_name.result)
-  location = random_shuffle.redisgeek.result[0]
-  tags     = merge(var.tags, { owner = local.sp_display_name })
+  location = var.location
+  tags     = merge(var.tags, { owner = data.azurerm_client_config.current.client_id })
 }
