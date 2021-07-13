842b8ac99388a0286302cf792b4e5a138cf91841
DaShaun <dashaun@redislabs.com>
2021-07-13 11:51:05 -0500

    update output to include updated provider exports

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,12 @@
 locals {
   redisgeek_config = {
-    cluster  = azurerm_redis_enterprise_cluster.redisgeek.*
-    database = azurerm_redis_enterprise_database.redisgeek.*
+    hostname   = azurerm_redis_enterprise_cluster.redisgeek.hostname
+    access_key = azurerm_redis_enterprise_database.redisgeek.primary_access_key
+    port       = "10000"
   }
-  secrets = {
-    primary_access_key = data.azurerm_key_vault_secret.primaryKey
-  }
-  sp_display_name = jsondecode(module.azure-base.redisgeek_config).sp
-  object_id = jsondecode(module.azure-base.redisgeek_config).oid
 }
 
 output "redisgeek_config" {
   value     = local.redisgeek_config
-  sensitive = false
-}
-
-output "secrets" {
-  value = local.secrets
   sensitive = true
 }
